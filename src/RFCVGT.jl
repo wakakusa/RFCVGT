832c0111da504d6549532866cf421cabb5b0656e
wakakusa <a.beginner.of.kdd.english@gmail.com>
2017-03-01 16:41:36 +0900

    Update RFCVGT.jl

--- src/RFCVGT.jl
+++ src/RFCVGT.jl
@@ -1,14 +1,14 @@
-module RFCVGT
-__precompile__()
+#module RFCVGT
+#__precompile__()
 # package code goes here
 using DataFrames,DecisionTree,MLBase
 
-export build_forest,apply_forest,gridtune
-export unievalfun,estfun,evalfun,gridsearch,rfcvgt
+#export build_forest,apply_forest,gridtune
+#export unievalfun,estfun,evalfun,gridsearch,rfcvgt
 
 include("estfun.jl")
 include("unievalfun.jl")
 include("evalfun.jl")
 include("gridsearch.jl")
 include("rfcvgtfun.jl")
-end # module
+#end # module
