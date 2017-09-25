a7cd7b534d3033b5ceb4ec9b0272450fdccab5bb
wakakusa <a.beginnerof.kdd.english@gmail.com>
2017-09-26 00:56:20 +0900

    gridtune不具合修正
    decisiontreeでnfeature=1実行時、エラーが発生する。エラーが出力されるまでに実行出来ることは実行するように変更

--- src/rfcvgtfun.jl
+++ src/rfcvgtfun.jl
@@ -5,11 +5,9 @@ function searchparm(Y,X;nfeature=0,ntree=[500,1000],nrate=[0.7],CV_n=5)
   CV_n=CV_n;
 
   #ランダムフォレストによる予測モデルの構築およびクロスバリデーションの実行
-  # estfun(nfeature,ntree,nrate)
   estfun=Estfun(Ytraining,Xtraining,CV_n)
 
   #評価関数の定義
-  # evalfun(scores)
   evalfun=Evalfun(Ytraining,Xtraining,CV_n)
 
   #パラメータの自動算定
@@ -17,13 +15,16 @@ function searchparm(Y,X;nfeature=0,ntree=[500,1000],nrate=[0.7],CV_n=5)
     if(Integer(round(size(Xtraining)[2]/3,0)) == 0)
       nfeature=[1,Integer(round(sqrt(size(Xtraining)[2]),0)) ,size(Xtraining)[2]]
     else
-      nfeature=[1,Integer(round(size(Xtraining)[2]/3,0)),Integer(round(sqrt(size(Xtraining)[2]),0)),size(Xtraining)[2]]
+      point1=Integer(round(size(Xtraining)[2]/3,0))
+      point2=Integer(round(sqrt(size(Xtraining)[2]),0))
+      nfeature=[1,2,Integer(round((point1+2)/2)),point1,Integer(round((point1+point2)/2)),point2,Integer(round((size(Xtraining)[2]+point2)/2)),size(Xtraining)[2]]
     end
 
-     #ダブリを除去
+     #特徴量の重複を除去
     nfeature=DataFrames.DataFrame(ind=nfeature)
     nfeature=DataFrames.by(nfeature, :ind, df -> size(df, 1))
     nfeature=convert(Array,nfeature[:,:ind])
+    nfeature=sort(nfeature,rev=true)
 
   end
 
